183cd5cc6b57687adeaf26402f5b84c3e479bbc0
FCCNhviana <hugo.viana@fccn.pt>
2016-05-23 15:20:27 +0100

    first commit

---
+++ src/main/java/pt/arquivo/logs/arquivo/IClickProcessor.java
@@ -0,0 +1,9 @@
+package pt.arquivo.logs.arquivo;
+
+/**
+ * Process clicks
+ * @author Miguel Costa
+ */
+public interface IClickProcessor {
+	ClickEntry exec(String queryPartsField0, String queryPartsField1);
+}
